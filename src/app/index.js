96bf9ceb6be62ffda3603f72109eff5fa62403d2
Vincent Prouillet <vincent@wearewizards.io>
2015-08-10 12:58:58 +0100

    Complete rebranding!

--- src/app/index.js
+++ src/app/index.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <p>Hi!</p>
+        <h1>A fake kanban app</h1>
         <ListContainer />
       </div>
     );
