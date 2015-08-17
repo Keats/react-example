8a76204aba6239809249959d668fdab7c9fb803e
Vincent Prouillet <vincent@wearewizards.io>
2015-08-17 13:17:06 +0100

    One less deps on gulp + react 0.14 beta3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
-var $ = require("gulp-load-plugins")();
+var autoprefixer = require("gulp-autoprefixer");
+var sass = require("gulp-sass");
 
 var isProd = process.env.NODE_ENV === "production";
 var outputFolder = isProd ? "./dist" : "./build";
@@ -7,8 +8,8 @@ var sassFiles = "src/style/**/*.scss"
 
 gulp.task("sass", function() {
   return gulp.src(sassFiles)
-    .pipe($.sass().on("error", $.sass.logError))
-    .pipe($.autoprefixer())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest(outputFolder))
 });
 
