e2c753382fb1cc0eaf0fbdf77bc91173ee2899db
Vincent Prouillet <vincent@wearewizards.io>
2015-08-17 18:00:47 +0100

    Start adding component tests, not really working

---
+++ src/tests/components/addForm_tests.js
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import React from "react";
+
+import AddForm from "../../app/components/addForm";
+
+
+describe("Components: addForm", () => {
+  let TestUtils;
+
+  beforeEach(() => {
+    TestUtils = require("react/lib/ReactTestUtils");
+  });
+
+  it("should render", () => {
+    const addForm = TestUtils.renderIntoDocument(<AddForm placeholder="hey" callback={() => {}} />);
+    expect(addForm).to.exist;
+  });
+
+  it("should call the callback with the input value", () => {
+    expect(true).to.be.true;
+  });
+});
