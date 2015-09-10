9cfa35fda6cc053f46c6d103fbd4240c72772a4b
Vincent Prouillet <vincent@wearewizards.io>
2015-09-10 14:48:44 +0100

    Update some deps including redux-react-router

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,15 @@
-import React, { PropTypes } from "react";
+import React from "react";
 import ReactDom from "react-dom";
-import { Router, Route, Link } from "react-router";
-import { history } from "react-router/lib/BrowserHistory";
-import { reduxRouteComponent } from "redux-react-router";
 
-import store from "./store";
-import ListContainer from "./components/listContainer";
+import { Provider } from "react-redux";
+import { ReduxRouter } from "redux-react-router";
+
+import configureStore from "./store";
 
 
-class App extends React.Component {
+const store = configureStore();
+
+class Root extends React.Component {
   renderDevTools() {
     if (__PRODUCTION__) {
       return null;
@@ -16,44 +17,22 @@ class App extends React.Component {
 
     const { DevTools, DebugPanel, LogMonitor } = require("redux-devtools/lib/react");
     return (
-      <DebugPanel top right bottom key="debugPanel">
+      <DebugPanel top right bottom>
         <DevTools store={store} monitor={LogMonitor}/>
       </DebugPanel>
     );
   }
 
   render() {
-    let content = <ListContainer />;
-    if (this.props.children) {
-      content = <p>Check the github repo: https://github.com/Keats/react-example</p>;
-    }
     return (
       <div>
-        <h1>A fake kanban app</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-        <hr/>
-        {content}
+        <Provider store={store}>
+          <ReduxRouter />
+        </Provider>
         {this.renderDevTools()}
       </div>
     );
   }
 }
 
-App.propTypes = {
-  children: PropTypes.element,
-};
-
-const routes = (
-  <Router history={history}>
-    <Route component={reduxRouteComponent(store)}>
-      <Route path="/" component={App}>
-        <Route path="about" component={App} />
-      </Route>
-    </Route>
-  </Router>
-);
-
-ReactDom.render(routes, document.getElementById("container"));
+ReactDom.render(<Root />, document.getElementById("container"));
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import {
   createStore, applyMiddleware, combineReducers, compose
 } from "redux";
-import { routerStateReducer } from "redux-react-router";
+import {
+  routerStateReducer,
+  reduxReactRouter
+} from "redux-react-router";
+import createHistory from "history/lib/createBrowserHistory";
 import thunk from "redux-thunk";
+
 import * as reducers from "./reducers/index";
+import routes from "./routes";
 
 
 const reducer = combineReducers({
@@ -15,17 +21,38 @@ const middlewares = [thunk];
 
 let finalCreateStore;
 
+const reduxRouter = reduxReactRouter({
+  routes,
+  createHistory,
+});
+
 if (__PRODUCTION__) {
-  finalCreateStore = applyMiddleware(...middlewares)(createStore);
+  finalCreateStore = compose(
+    applyMiddleware(...middlewares),
+    reduxRouter
+  )(createStore);
 } else {
   const { devTools, persistState } = require("redux-devtools");
   finalCreateStore = compose(
     applyMiddleware(...middlewares),
+    reduxRouter,
     devTools(),
-    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
-    createStore
-  );
+    persistState(window.location.href.match(/[?&]debug_session=([^&])\b/))
+  )(createStore);
 }
-export default finalCreateStore(reducer);
+
+export default function configureStore(initialState) {
+  const store = finalCreateStore(reducer, initialState);
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("./reducers", () => {
+      const nextReducer = require("./reducers");
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
 
 
